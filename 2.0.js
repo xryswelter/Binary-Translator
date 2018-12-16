372149f815738a073b5d1a9cc7f7b213965e080f
Xrys <xryswelter@gmail.com>
2018-12-16 13:14:20 -0800

    updated including reverse process

---
+++ 2.0.js
@@ -0,0 +1,98 @@
+// Array for objects that need to be append to element
+let origin = [];
+let translation = [];
+let state = 'etb'
+
+//Setting State for Translator function
+window.onload = () => {
+    status();
+    function status() {
+        let buttons = document.getElementById('buttons');
+        btn = buttons.getElementsByClassName('btn');
+        for (var i = 0; i < btn.length; i++) {
+            btn[i].addEventListener("click", function () {
+                var current = document.getElementsByClassName("active");
+                current[0].className = current[0].className.replace(" active", "");
+                this.className += " active";
+                state = this.value;
+                console.log(state)
+                spanText = (state == 'etb') ? 'English to Binary' : 'Binary to English';
+                document.getElementById('state').innerHTML = spanText;
+            });
+        }
+    }
+}
+
+
+
+//Upon Submit Button being pressed function occurs
+document.getElementById('submit').onclick = function () {
+    origin = [];
+    translation = [];
+    // Parse out sentence
+    let original = document.getElementById('original').value.split('');
+    console.log(original);
+    (state == 'etb') ? compare(original) : revert(original);
+};
+
+//*Functions for English to Binary Code
+function compare(original) {
+    let originL = document.getElementById('originL');
+    let trans = document.getElementById('trans');
+    originL.innerHTML = '';
+    trans.innerHTML = '';
+    console.log(original);
+    //   Clear Translation boxes
+    translation = [];
+    // Running through Array of characters and letters to find correct value
+    for (let j = 0; j < original.length; j++) {
+        //Native Code to translate letters to binary
+        let letterTrans = original[j].charCodeAt().toString(2);
+        //Funciton to add letter object to the translation array
+
+        appendToTranslation(original[j], letterTrans);
+    }
+    // Adding translation array to translation object
+    console.log(origin, translation)
+    originL.innerHTML = origin.join('');
+    trans.innerHTML = translation.join('');
+    console.log(originL, trans)
+}
+
+// Function to create letter object for front end display
+function appendToTranslation(original, binary) {
+    //Display Original Letter Element 
+    let originalLetter = document.createElement('div');
+    originalLetter.classList.add('originalLetter');
+    //Display Translated Letter Element
+    let translate = document.createElement('div');
+    translate.classList.add('translate');
+
+    //Appending Each element to appropriate object
+    origin.push(`${original}<br>`);
+    console.log(binary.length);
+    binary = (binary.length == 7) ? `0${binary}` : `00${binary}`
+    translation.push(`${binary}<br>`);
+}
+
+//* Functions for the Binary to English Translator
+function revert(original) {
+    original = original.join('')
+    console.log(original)
+    translateArray = original.split(' ')
+    console.log(translateArray)
+
+    tString = '';
+    translateArray.map(t => { tString += String.fromCharCode(parseInt(t, 2)) })
+    console.log(tString)
+    console.log(original)
+    originalArray = original.split(' ');
+    let oText = ''
+    originalArray.forEach(function (element) { oText += (`${element}<br>`) })
+    console.log(oText)
+
+    let originL = document.getElementById('originL');
+    let trans = document.getElementById('trans');
+    originL.innerHTML = oText;
+    trans.innerHTML = tString
+}
